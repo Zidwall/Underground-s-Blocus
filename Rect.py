8c42b6cb7eca3dd2e3226bda84686361c53a56eb
Zidwall <sznmaxi@gmail.com>
2025-04-26 21:58:57 +0200

    Add files via upload

---
+++ Rect.py
@@ -0,0 +1,34 @@
+import random
+from variable import variable
+variable = variable()
+class Rect:
+    def __init__(self):
+        self.giant_ecart = 710
+        self.x = 1300
+        self.y1 = 650
+        self.y2 = -60
+        self.largeur = 250
+        self.hauteur1 = 530
+        self.hauteur_bonus = 328
+        self.hauteur2 = 640
+        self.speed = 3
+        self.red = 255
+        self.blue = 255
+        self.green = 255
+    def MoveRect(self):
+        self.y1 = random.randint(280, 640)
+        self.x = 1300
+        self.red = 0
+        self.green = 0
+        self.blue = 0
+        self.giant_ecart -= 1
+    def initial(self):
+        self.x = 1300
+        self.y1 = 650
+        self.y2 = -60
+        self.red = 255
+        self.blue = 255
+        self.green = 255
+        self.speed = 3
+        self.giant_ecart = 710
+
---
+++ personnage.py
@@ -0,0 +1,48 @@
+import pygame
+
+
+
+pygame.init()
+from variable import variable
+
+variable = variable()
+screen = pygame.display.set_mode((1000, 800))
+class Perso:
+    def __init__(self):
+        self.x = 30
+        self.y = 594
+        self.vitesse = 4
+        self.largeur = 25
+        self.hauteur = 38
+        self.lancement = None
+    def haut(self):
+            if not variable.inversed_gravity:
+                self.y -= self.vitesse
+    def haut_world2(self):
+        self.y -= 132
+    def bas(self):
+        self.y += self.vitesse
+    def bas_world2(self):
+            self.y += 132
+
+    def droite(self):
+        self.x += self.vitesse
+
+
+    def gauche(self):
+        self.x -= self.vitesse
+        if self.x < -5:
+            self.x += self.vitesse
+    def initial(self):
+        self.x = 30
+        self.y = 594
+        self.vitesse = 4
+        self.largeur = 25
+        self.hauteur = 38
+
+
+    def nouvelles_dimensions_joueur(self):
+        for loop in range(30):
+            self.largeur -= 0.1
+        for loop in range(30):
+            self.hauteur -= 0.2
